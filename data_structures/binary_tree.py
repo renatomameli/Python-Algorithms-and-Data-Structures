ec2870d125c2dc93a8e6aea34fd90364d023abe2
renatomameli <renatomamel410@gmail.com>
2023-09-26 23:02:52 +0200

    binary tree and graph

--- data_structures/binary_tree.py
+++ data_structures/binary_tree.py
@@ -16,3 +16,21 @@ def in_order_traversal(node):
         print(node.value)
         in_order_traversal(node.right)
 
+
+# Big O(n)
+def pre_order_traversal(node):
+    if node is not None:
+        print(node.value)
+        pre_order_traversal(node.left)
+        pre_order_traversal(node.right)
+
+
+# Big O(n)
+def post_order_traversal(node):
+    if node is not None:
+        post_order_traversal(node.left)
+        post_order_traversal(node.right)
+        print(node.value)
+
+# Level order (breadth-first)
+
---
+++ data_structures/graph.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, name=None, children=None):
+        self.name = name
+        self.children = children
+
+
+class Graph:
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+
